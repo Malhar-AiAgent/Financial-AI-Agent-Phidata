ad41a0710ad514434b7c5716bacebc2507f26034
Malhar-AiAgent <malharubhe03@gmail.com>
2025-08-25 10:24:51 +0530

    Update check.py

--- check.py
+++ check.py
@@ -6,5 +6,6 @@ class GroqEmbedder:
         self.client = GroqClient(api_key=api_key)
 
     def get_embedding_and_usage(self, text: str):
-        response = self.client.embeddings.create(input=text, model="groq-embedding-ada-002")
+        response = self.client.embeddings.create(input=text, model="Groq-model to be used")
         return response['data'][0]['embedding'], response['usage']
+
